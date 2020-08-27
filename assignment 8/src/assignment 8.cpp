1e1b51f94c128b4af940acd76b859f64845b7790
zakuraevs <zakuraevs@gmail.com>
2020-08-27 20:29:18 +0300

    Added assignment 8

--- assignment 8/src/assignment 8.cpp
+++ assignment 8/src/assignment 8.cpp
@@ -1,15 +1,26 @@
-//============================================================================
-// Name        : assignment.cpp
-// Author      : Sergey Zakuraev
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+
 
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	int a, b;
+
+	cin >> a >> b;
+
+
+	while (a > 0 && b > 0){
+		if( a > b ) {
+			a = a % b;
+		} else {
+			b = b % a;
+		}
+	}
+
+
+
+	cout << a + b;
+
 	return 0;
 }
