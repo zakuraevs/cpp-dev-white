78b9ec0911a68dd3fdd2c942279fd2d618e04ede
zakuraevs <zakuraevs@gmail.com>
2020-09-04 10:54:41 +0300

    Added assignment 12

---
+++ assignment 12/src/assignment 12.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+bool IsPalindrom(string s) {
+    for (int i = 0; i < s.size() / 2; ++i) {
+        if (s[i] != s[s.size() - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<string> PalindromFilter(vector<string> words, int minLength) {
+
+	vector<string> res;
+
+	for(auto i : words) {
+		if(i.size() >= minLength && IsPalindrom(i) ) {
+			res.push_back(i);
+		}
+	}
+
+
+	return res;
+}
+
+
+int main() {
+
+	vector<string> vect;
+
+	vect.push_back("abacaba");
+	vect.push_back("aba");
+
+	vector<string> res = PalindromFilter(vect, 5);
+
+	for(auto i : res) {
+		cout << i << endl;
+	}
+
+	return 0;
+}
